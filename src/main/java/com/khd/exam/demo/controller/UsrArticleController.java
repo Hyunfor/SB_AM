39a2345b4d8e474ceaa72e39703a0c44de22d81c
Hyunfor <navel5148@gmail.com>
2022-12-02 19:27:17 +0900

    wrticleArticle 메서드를 활용해서 게시물 테스트 데이터 10개 생성

--- src/main/java/com/khd/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khd/exam/demo/controller/UsrArticleController.java
@@ -17,17 +17,35 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.lastArticldId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody 
-	public Article doAdd(String title, String body) {
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+		
+	}
+	
+	private Article writeArticle(String title, String body) { // doAdd, makeTestData 의 중복코드를 하나로 합치기
 		int id = lastArticldId + 1;
+		
 		Article article = new Article(id, title, body);
 		
 		articles.add(article);
 		lastArticldId = id;
 		
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody 
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
 		return article;
 	}
 	
