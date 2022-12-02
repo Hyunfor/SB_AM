5e986b22e70dc48e8290dca053345470c455db8d
Hyunfor <navel5148@gmail.com>
2022-12-02 20:02:41 +0900

    서비스 메서드로 게시물 삭제기능 구현

--- src/main/java/com/khd/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khd/exam/demo/controller/UsrArticleController.java
@@ -21,6 +21,8 @@ public class UsrArticleController {
 		makeTestData();
 	}
 	
+	
+// 서비스 메서드
 	private void makeTestData() {
 		for(int i = 1; i <= 10; i++) {
 			String title = "제목" + i;
@@ -29,6 +31,17 @@ public class UsrArticleController {
 			writeArticle(title, body);
 		}
 		
+	}
+	
+	private Article getArticle(int id) {
+		
+		for(Article article : articles) {
+			if(article.getId() == id) {// 데이터 변질을 막기위해 get, set을 사용
+				return article;
+			} 
+		}
+		
+		return null;
 	}
 	
 	private Article writeArticle(String title, String body) { // doAdd, makeTestData 의 중복코드를 하나로 합치기
@@ -41,7 +54,22 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	
+	private void modifyArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+		
+	}
 
+	
+// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody 
 	public Article doAdd(String title, String body) {
@@ -54,6 +82,47 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 		
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody 
+	public String doDelete(int id) {
+		
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		
+		deleteArticle(id);
+		
+		return id + "번 게시물을 삭제했습니다.";
+	}
+	
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody 
+	public String doModify(int id) {
+		
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id);
+		
+		return id + "번 게시물을 수정했습니다.";
+	}
+
+
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody 
+	public String getArticle() {
+		
+		wrtieArticle(id);
+		
+		return id + "번 게시물을 게시했습니다.";
 	}
 
 	
